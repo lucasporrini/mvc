dff7a2389580d90b63714f7cb91988f5b74e1cf6
lucasporrini <50617944+lucasporrini@users.noreply.github.com>
2024-01-15 16:49:50 +0100

    Feature: added chart.js to display chart into the admin

---
+++ web/src/public/assets/js/chart.js
@@ -0,0 +1,37 @@
+const ctx = document.getElementById('productChart');
+
+const colors = {
+    'blue-50': '#eff6ff',
+    'blue-100': '#dbeafe',
+    'blue-200': '#bfdbfe',
+    'blue-300': '#93c5fd',
+    'blue-400': '#60a5fa',
+    'blue-500': '#3b82f6',
+    'blue-600': '#2563eb',
+    'blue-700': '#1d4ed8',
+    'blue-800': '#1e40af',
+    'blue-900': '#1e3a8a',
+    'blue-950': '#172554'
+};
+
+const data = {
+    labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+    ],
+    datasets: [{
+        data: [300, 50, 100],
+        backgroundColor: [
+            colors['blue-200'],
+            colors['blue-400'],
+            colors['blue-600']
+        ],
+        hoverOffset: 4
+    }]
+}
+
+new Chart(ctx, {
+    type: 'doughnut',
+    data: data
+});
--- web/src/public/partials/admin/scripts.php
+++ web/src/public/partials/admin/scripts.php
@@ -1,1 +1,3 @@
 <script src="https://cdn.jsdelivr.net/npm/preline@2.0.3/dist/preline.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="<?= BASE_URL ?>public/assets/js/chart.js"></script>
